60f4d5b5426f88f321432eab68d8fed9b00a53fb
BenasB <29711974+BenasB@users.noreply.github.com>
2025-04-23 17:30:37 +0200

    Add solid wall scenario

--- Bomberman.Core/Scenario.cs
+++ Bomberman.Core/Scenario.cs
@@ -1,20 +1,11 @@
-﻿namespace Bomberman.Core;
+﻿using Bomberman.Core.Tiles;
+
+namespace Bomberman.Core;
 
 public class Scenario
 {
     public required TileMap TileMap { get; init; }
     public required GridPosition[] StartPositions { get; init; }
-
-    public static Scenario Empty =>
-        new()
-        {
-            TileMap = new TileMap(17, 11).WithBorder(),
-            StartPositions =
-            [
-                new GridPosition(Row: 5, Column: 6),
-                new GridPosition(Row: 5, Column: 15),
-            ],
-        };
 
     public static Scenario Default =>
         new()
@@ -33,4 +24,37 @@ public class Scenario
                 new GridPosition(Row: 5, Column: 12),
             ],
         };
+
+    public static Scenario Empty =>
+        new()
+        {
+            TileMap = new TileMap(17, 11).WithBorder(),
+            StartPositions =
+            [
+                new GridPosition(Row: 5, Column: 6),
+                new GridPosition(Row: 5, Column: 15),
+            ],
+        };
+
+    public static Scenario SolidWall
+    {
+        get
+        {
+            var tileMap = new TileMap(17, 11).WithBorder();
+            for (int i = 2; i < tileMap.Height - 2; i++)
+            {
+                tileMap.PlaceTile(new WallTile(new GridPosition(i, tileMap.Width - 3)));
+            }
+
+            return new Scenario()
+            {
+                TileMap = tileMap,
+                StartPositions =
+                [
+                    new GridPosition(Row: 5, Column: 6),
+                    new GridPosition(Row: 5, Column: 15),
+                ],
+            };
+        }
+    }
 }
