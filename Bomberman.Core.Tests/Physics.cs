fc25450d46a6f1ea471ba52565057a0580932e7f
BenasB <29711974+BenasB@users.noreply.github.com>
2025-05-11 23:54:06 +0300

    Standing on bomb physics test

--- Bomberman.Core.Tests/Physics.cs
+++ Bomberman.Core.Tests/Physics.cs
@@ -32,6 +32,36 @@ public class Physics
     }
 
     [Theory]
+    [InlineData(16)]
+    [InlineData(16 * 2)]
+    [InlineData(16 * 3)]
+    [InlineData(16 * 4)]
+    [InlineData(16 * 6)]
+    [InlineData(16 * 10)]
+    [InlineData(16 * 100)]
+    public void PlayerMovement_BigVelocityNextToWallStandingOnBomb_DoesNotWalkThroughSolidWall(
+        int deltaTimeMs
+    )
+    {
+        var deltaTime = TimeSpan.FromMilliseconds(deltaTimeMs);
+
+        // Player is right next to a wall (also standing on a bomb) and moving with any velocity towards it should not change the players position
+        var startingPlayerPosition = new GridPosition(0, 0);
+        var wallPosition = new GridPosition(0, 1);
+
+        var tileMap = new TileMap(3, 1);
+        tileMap.PlaceTile(new WallTile(wallPosition));
+        tileMap.PlaceTile(new BombTile(startingPlayerPosition, tileMap, 1));
+        var player = new Player(startingPlayerPosition, tileMap);
+
+        player.SetMovingDirection(Direction.Right);
+        player.Update(deltaTime);
+        player.Update(TimeSpan.Zero);
+
+        Assert.Equal((Vector2)startingPlayerPosition, player.Position);
+    }
+
+    [Theory]
     [InlineData(1)]
     [InlineData(2)]
     [InlineData(3)]
@@ -49,6 +79,7 @@ public class Physics
 
         player.SetMovingDirection(Direction.Right);
         player.Update(deltaTime);
+        player.Update(TimeSpan.Zero);
 
         Assert.Equal(
             (Vector2)(wallPosition with { Column = wallPosition.Column - 1 }),
